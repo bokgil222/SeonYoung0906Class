c7501eaeadca340f23a7edf08c6f65c5cade348f
bokgil222 <rhyme6002@gmail.com>
2021-10-25 22:28:21 +0900

    Project 6 Contact ver06

---
+++ java project/FirstJavaProject/src/ver06/BadNameInputException.java
@@ -0,0 +1,8 @@
+package ver06;
+
+public class BadNameInputException extends Exception {
+
+	public BadNameInputException(String message) {
+		super(message);
+	}
+}
---
+++ java project/FirstJavaProject/src/ver06/PhoneNumberPatternException.java
@@ -0,0 +1,8 @@
+package ver06;
+
+public class PhoneNumberPatternException extends Exception {
+	
+	public PhoneNumberPatternException() {
+		super("전화번호 형식에 맞지 않습니다.(예: 000-0000-0000)");
+	}
+}
