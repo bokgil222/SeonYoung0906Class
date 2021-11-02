fc3d97edf101f5af58d946ec595cfb34f75d8f94
bokgil222 <rhyme6002@gmail.com>
2021-11-02 23:20:24 +0900

    Update 20211029.sql

--- sql/20211029.sql
+++ sql/20211029.sql
@@ -1,3 +1,8 @@
+-- 2021.10.29
+
+desc dual;
+select * from dual;
+
 -- 함수: 단일행 함수, 집합 함수
 
 -- 단일행 함수: 숫자, 문자, 날짜, 변환
@@ -16,11 +21,11 @@ select mod(10, 3) from dual;
 -- 문자 함수
 select LOWER('MR. SCOTT MCMILLAN') "Lowercase" from dual;
 select ename, lower(ename) from emp;
-select concat('저는', '손흥민입니다.') from dual;
-select ename || 'is a ' from emp;
+select concat('저는 ', '손흥민입니다.') from dual;
+select ename || ' is a' from emp;
 select concat('나의 이름은 ', ename) from emp;
 select LPAD('Page 1',15,'*') from dual; -- *********Page 1
-select LPAD('Page 1',15,'*') from dual;
+select RPAD('Page 1',15,'*') from dual;
 select rpad('001111-3', 14, '*') from dual;
 
 select SUBSTR('ABCDEFG',3,4) from dual;
@@ -75,6 +80,79 @@ select to_char(100000.123,'L9,999,999.9') from dual;
 select sal, to_char(sal*1200, 'L9,999,999') from emp;
 
 -- 문자 -> 숫자 to_number(원본, 패턴)
+select to_number('100,000', '9,999,999') + 100000 from dual;
 
+-- sxitch case와 유사한 decode 함수
+-- emp 테이블에서 사원 이름, 부서번호ㅡ 부서이름 출력
+select * from dept;
+select * from emp;
+select ename, deptno,
+       decode(deptno,
+           10, 'ACCOUNT',
+           20, 'RESERACH',
+           30, 'SALES',
+           40, 'OPERATION'
+       ) as dname
+from emp
+;
 
+-- 직급에 따라 급여를 인상
+-- 직급이 'ANALYST'인 사원은 5%,
+--       'SALESMAN'인 사원은 10%,
+--       'MANAGER'인 사원은 15%,
+--       'CLERK'인 사원은 20% 인상
+select ename, job, sal,
+        decode(job, 
+             'ANALYST', sal*1.05,
+             'SALESMAN', sal*1.1,
+             'MANAGER', sal*1.15,
+             'CLERK', sal*1.2
+             ) as upSal
+from emp
+;
 
+-- case when then: if else if 와 유사 -> 조건식을 = 이외의 비교 연산을 할 수 있다.
+select ename, sal
+case
+when sal > = 3000 then sal*1.1
+when sal < 3000 then sal*2
+end as upsal
+from emp
+;
+
+------------------------------------------------------------------
+-- 집합 함수 (그룹 함수)
+-- 하나의 행의 컬럼이 대상이 아니고, 행 그룹의 컬럼들(집합)이 대상
+------------------------------------------------------------------
+
+select to_char(sum(sal)*1200, 'L999,999,999) as "월 급여 총액",
+to_char(round(avg(sal)*1200), 'L999,999,999) as "월 급여 평균",
+count(*) as "총 사원의 수",
+count(comm) as "커미션 등록 수"
+sum(comm) as "커미션 합",
+avg(comm) as "커미션 평균",
+max(sal) as "가장 높은 급여",
+min(sal) as "가장 낮은 급여"
+from emp
+;
+
+// 사원들의 업무의 개수
+select job from emp;
+
+select deptno,
+coutn(*) as "사원 수",
+sum(sal) as "급여 총합",
+trunc(avg(sal)) as "급여 평균",
+max(sal) as "최대 급여",
+min(sal) as "최소 급여",
+count(comm) as "커미션 대상자 수"
+nvl(sum(comm), 0) as "커미션 총합",
+nvl(avg(comm), 0) as "커미션 평균"
+
+from emp
+-- where deptno != 20
+group by deptno
+-- having avg sal >= 2000
+having max(sal) > 2900
+order by deptno
+;
