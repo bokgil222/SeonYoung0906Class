b34af9b937f2888285ea9f7d37956dc4e3b808c9
서선영 <rhyme6002@gmail.com>
2021-11-08 19:42:17 +0900

    JDBC Dept Manager

---
+++ JDBC/src/dept/JdbcUtil.java
@@ -0,0 +1,30 @@
+package dept;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcUtil {
+
+	public static void close(Connection conn) {
+		
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} 
+		}
+	}
+}
