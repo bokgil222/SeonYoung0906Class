073d1a591e5c62eed9df474312a8f5f2d135a673
bokgil222 <rhyme6002@gmail.com>
2021-11-12 10:56:28 +0900

    Update menu.sql

--- sql/javakingProject/menu.sql
+++ sql/javakingProject/menu.sql
@@ -7,16 +7,20 @@ CREATE TABLE menu (
 	mid NUMBER(6) CONSTRAINT mid_pk PRIMARY KEY NOT NULL, /* 메뉴번호 */
 	mname VARCHAR(50) NOT NULL, /* 메뉴명 */
 	mprice INTEGER NOT NULL, /* 메뉴가격 */
-	mtype VARCHAR2(10) NOT NULL /* 메뉴종류 */
+	mtype number(1) NOT NULL, /* 메뉴종류 */
+    mcount INTEGER DEFAULT 1 NOT NULL /* 메뉴수량 */
 );
-insert into menu values (1, '불고기버거', 3500, 10);
-insert into menu values (2, '상하이버거', 4000, 10);
-insert into menu values (3, '후렌치후라이', 2000, 20);
-insert into menu values (4, '치즈볼', 3000, 20);
-insert into menu values (5, '콜라', 1000, 30);
-insert into menu values (6, '사이다', 1000, 30);
-insert into menu values (7, '환타', 1000, 30);
-insert into menu values (8, '아이스크림', 1000, 40);
-insert into menu values (9, '쉐이크', 1000, 40);
+insert into menu (MID, MNAME, MPRICE, MTYPE) values (1, '불고기버거', 3500, 1);
+insert into menu (MID, MNAME, MPRICE, MTYPE) values (2, '상하이버거', 4000, 1);
+insert into menu (MID, MNAME, MPRICE, MTYPE) values (3, '후렌치후라이', 2000, 2);
+insert into menu (MID, MNAME, MPRICE, MTYPE) values (4, '치즈볼', 3000,  2);
+insert into menu (MID, MNAME, MPRICE, MTYPE) values (5, '아이스크림', 1000, 2);
+insert into menu  (MID, MNAME, MPRICE, MTYPE) values (6, '쉐이크', 1000, 2);
+insert into menu (MID, MNAME, MPRICE, MTYPE)values (7, '콜라', 1000, 3);
+insert into menu values (8, '사이다', 1000, 3);
+insert into menu values (9, '환타', 1000, 3);
+
 
 select * from menu;
+COMMIT;
+rollback;
