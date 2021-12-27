6c6c07ea32e152c7ae20a402ec574a448f274d44
bokgil222 <rhyme6002@gmail.com>
2021-12-28 01:04:56 +0900

    Open Project - ajax

---
+++ jsp project/OpenProject/src/guestbook/command/GuestBookWriteAjaxCommandImpl.java
@@ -0,0 +1,19 @@
+package guestbook.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import command.Command;
+
+public class GuestBookWriteAjaxCommandImpl implements Command {
+
+	@Override
+	public String getPage(HttpServletRequest request, HttpServletResponse response) {
+		
+		if(request.getMethod().contentEquals("POST")) {
+			request.setAttribute("result", GuestBookWriteService.getInstance().writeArticle(request, response));
+		}
+		
+		return "/WEB-INF/views/guestbook/result.jsp";
+	}
+}
---
+++ jsp project/OpenProject/src/guestbook/command/GuestBookWriteCommandImpl.java
@@ -0,0 +1,28 @@
+package guestbook.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import command.Command;
+
+public class GuestBookWriteCommandImpl implements Command {
+
+	@Override
+	public String getPage(HttpServletRequest request, HttpServletResponse response) {
+
+		String view = null;
+		
+		if(request.getMethod().contentEquals("GET")) {
+			
+			view = "/WEB-INF/views/guestbook/writeform.jsp";
+			
+		} else if(request.getMethod().contentEquals("POST")) {
+			
+			request.setAttribute("result", GuestBookWriteService.getInstance().writeArticle(request, response));
+			
+			view = "/WEB-INF/views/guestbook/write.jsp";
+		}
+		
+		return view;
+	}
+}
